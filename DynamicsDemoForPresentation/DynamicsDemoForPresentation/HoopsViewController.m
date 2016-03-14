5e04736cc2fdbf7816a72d49d3f3c2de07e52156
andynovak12 <andynovak12@gmail.com>
2016-03-14 17:42:01 -0400

    tim's gonna break stuff

--- DynamicsDemoForPresentation/DynamicsDemoForPresentation/HoopsViewController.m
+++ DynamicsDemoForPresentation/DynamicsDemoForPresentation/HoopsViewController.m
@@ -8,7 +8,7 @@
 
 #import "HoopsViewController.h"
 #import <QuartzCore/QuartzCore.h>
-
+//#import <ScoreboardLabel/ScoreboardLabel.swift>
 
 
 @interface HoopsViewController () <UICollisionBehaviorDelegate>
@@ -17,7 +17,10 @@
 @property (weak, nonatomic) IBOutlet UIButton *basketball;
 @property (strong, nonatomic) UIPushBehavior *pushBehavior;
 @property (weak, nonatomic) IBOutlet UIView *frontOfRim;
-@property (weak, nonatomic) IBOutlet UIView *backboard;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (nonatomic) NSUInteger score;
+@property (weak, nonatomic) IBOutlet UIImageView *hoop;
+@property (nonatomic) BOOL justScorred;
 
 @property (nonatomic) CFAbsoluteTime lastTime;
 @end
@@ -30,47 +33,81 @@
     
     self.basketball.layer.cornerRadius = self.basketball.frame.size.height/2;
     self.basketball.clipsToBounds = YES;
-//    [self.basketball.widthAnchor constraintEqualToConstant:50].active = YES;
+
+    
+    
+    //    [self.basketball.widthAnchor constraintEqualToConstant:50].active = YES;
 //        [self.basketball.heightAnchor constraintEqualToConstant:50].active = YES;
+    
+    
+    
+
+
+
 
     
     // Initialize the animator.
     self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
     
+   
     
+    //gravity for ball
+    UIGravityBehavior *gravityBehavior = [[UIGravityBehavior alloc] initWithItems:@[self.basketball]];
+    gravityBehavior.magnitude = 1.5;
+    [self.animator addBehavior:gravityBehavior];
     
     //create collisions
-    UICollisionBehavior *collisionBehavior = [[UICollisionBehavior alloc] initWithItems:@[self.basketball, self.frontOfRim, self.backboard]];
+    UICollisionBehavior *collisionBehavior = [[UICollisionBehavior alloc] initWithItems:@[self.basketball, self.frontOfRim]];
     //makes our main view’s bounds work as boundaries for the items that will collide.
     collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
     //dictates that all edges of items that participate to a collision will have the desired behavior.
     collisionBehavior.collisionMode = UICollisionBehaviorModeEverything;
+
+//    collisionBehavior.collisionMode = UICollisionBehaviorModeItems;
     //make view collisionDelegate
     collisionBehavior.collisionDelegate = self;
     [self.animator addBehavior:collisionBehavior];
     
 
+
     //set the basketballs properties
     UIDynamicItemBehavior *ballDynamic = [[UIDynamicItemBehavior alloc]initWithItems:@[self.basketball]];
-    ballDynamic.elasticity = 0.8;
+    ballDynamic.elasticity = 0.6;
     ballDynamic.resistance = 0;
     ballDynamic.allowsRotation = YES;
     [self.animator addBehavior:ballDynamic];
     
     //set the hoops properties
-    UIDynamicItemBehavior *hoopDynamic = [[UIDynamicItemBehavior alloc]initWithItems:@[self.frontOfRim, self.backboard]];
+    UIDynamicItemBehavior *hoopDynamic = [[UIDynamicItemBehavior alloc]initWithItems:@[self.frontOfRim]];
     hoopDynamic.anchored = YES;
     [self.animator addBehavior:hoopDynamic];
     
 //    [self repositionBall];
+//    NSLog(@"BALL CCROSSED: %d, ", [self ballCrossedHoop]);
+    
+    gravityBehavior.action = ^{
+        [self updateScore];
+    };
+    
+    
+    
+    
 }
 
 
 - (IBAction)ballSwipped:(UIPanGestureRecognizer*)recognizer {
     if (UIGestureRecognizerStateBegan) {
+        
+        NSLog(@"SELF JUST SCORRED IS BEING SET TO NO!!!!");
+        self.justScorred = NO;
+        
         NSLog(@"started");
     }
     if (UIGestureRecognizerStateEnded){
+        
+        
+        NSLog(@"GETTING CALLED!!!!!!!!!!!!!!!!!!!!!!!! stateEnded");
+        
         CGPoint velocity = [recognizer velocityInView:[recognizer.view superview]];
         // If needed: CGFloat slope = velocity.y / velocity.x;
         CGFloat angle = atan2f(velocity.y, velocity.x);
@@ -81,68 +118,70 @@
         NSLog(@"%f", angle);
     }
 
-    //this only registers swipes longer than 0.1
-//    UIGestureRecognizerState state = recognizer.state;
-//    CGPoint gestureVelocity = [recognizer velocityInView:[recognizer.view superview]];
-//    CGPoint finalSpeed = CGPointMake(0, 0);
-//    
-//    if ( state == UIGestureRecognizerStateBegan ){
-//        self.lastTime = CFAbsoluteTimeGetCurrent();
-//    }
-//    else if ( state == UIGestureRecognizerStateEnded ) {
-//        double curTime = CFAbsoluteTimeGetCurrent();
-//        double timeElapsed = curTime - self.lastTime;
-//        if ( timeElapsed > 0.05 )
-//            finalSpeed = gestureVelocity;
-//        else{
-//            finalSpeed = CGPointZero;
-//        }
-//        NSLog(@"%f", timeElapsed);
-//    }
-//
-    //if there is a final speed that is not 0
-//    if ((finalSpeed.x != 0) || (finalSpeed.y != 0)) {
-//        NSLog(@"SPEED %@",NSStringFromCGPoint(finalSpeed));
-//        CGFloat velocityAsFloat = sqrt(pow(finalSpeed.y,2)+pow(finalSpeed.x, 2));
-//        CGFloat angle = atan2f(finalSpeed.y, finalSpeed.x);
-//
-//        [self pushTheBallWithAngle:angle WithVelocity:velocityAsFloat];
-//    }
-//
-
 }
-//- (IBAction)resetTapped:(id)sender {
-//    NSLog(@"tapped");
-//    [self.animator removeBehavior:self.pushBehavior];
-//
-////    [self.animator removeAllBehaviors];
-//    [self repositionBall];
-//}
 
 -(void)pushTheBallWithAngle:(CGFloat)angle WithVelocity:(CGFloat)velocity{
-    //gravity for ball
-    UIGravityBehavior *gravityBehavior = [[UIGravityBehavior alloc] initWithItems:@[self.basketball]];
-    gravityBehavior.magnitude = 1.5;
     
-
     self.pushBehavior = [[UIPushBehavior alloc] initWithItems:@[self.basketball] mode:UIPushBehaviorModeInstantaneous];
+
     self.pushBehavior.magnitude = velocity/4000;
     self.pushBehavior.angle = angle;
     
+//    [self.animator addBehavior:self.pushBehavior];
     [self.animator addBehavior:self.pushBehavior];
-    [self.animator addBehavior:gravityBehavior];
+
     NSLog(@"pushed");
+    
 }
 
-//-(void)repositionBall{
-//    [self.view removeConstraints:self.view.constraints];
-//    self.basketball.translatesAutoresizingMaskIntoConstraints = NO;
-//    NSLog(@"repositioned");
-//    [self.basketball.widthAnchor constraintEqualToConstant:50].active = YES;
-//    [self.basketball.heightAnchor constraintEqualToConstant:50].active = YES;
-//    [self.basketball.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:100].active = YES;
-//    [self.basketball.leftAnchor constraintEqualToAnchor:self.view.leftAnchor constant:300].active = YES;
-//    
-//}
+
+
+-(BOOL)ballCrossedHoop{
+    
+    CGFloat delta = self.basketball.center.y - (self.hoop.center.y - self.hoop.frame.size.height/4);
+    CGFloat deltaNonNegative = delta < 0 ? delta * -1 : delta;
+    BOOL ballCrossedCenterY = deltaNonNegative < 30 ? YES : NO;
+    
+    BOOL ballLessThanRightHoop = (self.basketball.center.x < self.hoop.center.x +self.hoop.frame.size.width/2);
+    BOOL ballGreaterThanLeftHoop = (self.basketball.center.x > self.hoop.center.x - self.hoop.frame.size.width/2);
+    
+
+    
+    
+    if (_justScorred) {
+        
+        NSLog(@"JUST SCORED IS YESSSSSS\n\n\n\n");
+        
+        return  NO;
+        
+    } else {
+        
+
+    BOOL scoreeeee = ballCrossedCenterY &&  ballLessThanRightHoop &&  ballGreaterThanLeftHoop;
+        
+        if (scoreeeee) {
+            
+            NSLog(@"GOALLL");
+        }
+    return scoreeeee;
+        
+    }
+    
+    
+}
+
+-(void)updateScore{
+    
+    
+    BOOL ballCrossedHoopBool = [self ballCrossedHoop];
+    if (ballCrossedHoopBool) {
+
+        
+        NSLog(@"SCOREEEEEEE");
+        self.justScorred = YES;
+        self.score ++;
+        self.scoreLabel.text = [NSString stringWithFormat: @"%lu", self.score];
+    }
+}
 
 @end
